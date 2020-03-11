0624075484967190e88acbe7840068e3bf007df7
tiking76 <716yosiki1999@gmail.com>
2020-03-12 01:30:43 +0900

    create xibview

---
+++ sampleXibApp/sampleXibApp/SampleView.swift
@@ -0,0 +1,13 @@
+//
+//  SampleView.swift
+//  sampleXibApp
+//
+//  Created by 舘佳紀 on 2020/03/11.
+//  Copyright © 2020 yoshiki Tachi. All rights reserved.
+//
+
+import UIKit
+
+class SampleView: UIView {
+    
+}
